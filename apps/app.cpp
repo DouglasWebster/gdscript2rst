c048a3ff7b0cc7735845b70d37c376ad4f561cbb
Douglas Webster <douglas.webster2107@gmail.com>
2023-05-14 16:29:43 +0100

    reformated using .clang-format

--- apps/app.cpp
+++ apps/app.cpp
@@ -15,17 +15,21 @@ wxIMPLEMENT_APP(MyApp);
  */
 bool MyApp::OnInit()
 {
-  MainFrame *frame = new MainFrame(nullptr, wxID_ANY, "Godot GScript API to ReStructured Text",
-                                   wxDefaultPosition, wxSize(1024, 600));
+  MainFrame *frame =
+    new MainFrame(nullptr, wxID_ANY, "Godot GScript API to ReStructured Text", wxDefaultPosition, wxSize(1024, 600));
   frame->Show(true);
   frame->OnCheckPython();
 
   return true;
 }
 
-MainFrame::MainFrame(wxWindow *parent, wxWindowID id, const wxString &title, const wxPoint &pos,
-                     const wxSize &size, long style)
-    : wxFrame{parent, id, title, pos, size, style}
+MainFrame::MainFrame(wxWindow *parent,
+  wxWindowID id,
+  const wxString &title,
+  const wxPoint &pos,
+  const wxSize &size,
+  long style)
+  : wxFrame{ parent, id, title, pos, size, style }
 {
   logger = new wxLogStderr();
   wxLog::SetActiveTarget(logger);
@@ -35,33 +39,28 @@ MainFrame::MainFrame(wxWindow *parent, wxWindowID id, const wxString &title, con
   m_MainFrameMenu = new wxMenuBar(0);
   m_menuFile = new wxMenu();
   wxMenuItem *m_menuProjectNew;
-  m_menuProjectNew = new wxMenuItem(m_menuFile, wxID_NEW, wxString(wxEmptyString), wxEmptyString,
-                                    wxITEM_NORMAL);
+  m_menuProjectNew = new wxMenuItem(m_menuFile, wxID_NEW, wxString(wxEmptyString), wxEmptyString, wxITEM_NORMAL);
   m_menuFile->Append(m_menuProjectNew);
 
   wxMenuItem *m_menuFileOpen;
-  m_menuFileOpen = new wxMenuItem(m_menuFile, wxID_OPEN, wxString(wxEmptyString), wxEmptyString,
-                                  wxITEM_NORMAL);
+  m_menuFileOpen = new wxMenuItem(m_menuFile, wxID_OPEN, wxString(wxEmptyString), wxEmptyString, wxITEM_NORMAL);
   m_menuFile->Append(m_menuFileOpen);
 
   wxMenuItem *m_menuSave;
-  m_menuSave = new wxMenuItem(m_menuFile, wxID_SAVE, wxString(wxEmptyString), wxEmptyString,
-                              wxITEM_NORMAL);
+  m_menuSave = new wxMenuItem(m_menuFile, wxID_SAVE, wxString(wxEmptyString), wxEmptyString, wxITEM_NORMAL);
   m_menuFile->Append(m_menuSave);
 
   m_menuFile->AppendSeparator();
 
   wxMenuItem *m_menuFileClose;
-  m_menuFileClose = new wxMenuItem(m_menuFile, wxID_CLOSE, wxString(wxEmptyString),
-                                   wxEmptyString, wxITEM_NORMAL);
+  m_menuFileClose = new wxMenuItem(m_menuFile, wxID_CLOSE, wxString(wxEmptyString), wxEmptyString, wxITEM_NORMAL);
   m_menuFile->Append(m_menuFileClose);
 
   m_MainFrameMenu->Append(m_menuFile, wxT("File"));
 
   m_menuCheck = new wxMenu();
   wxMenuItem *menuCheckPython;
-  menuCheckPython = new wxMenuItem(m_menuCheck, wxID_ANY, wxString(wxT("Python...")),
-                                   wxEmptyString, wxITEM_NORMAL);
+  menuCheckPython = new wxMenuItem(m_menuCheck, wxID_ANY, wxString(wxT("Python...")), wxEmptyString, wxITEM_NORMAL);
   m_menuCheck->Append(menuCheckPython);
 
   m_MainFrameMenu->Append(m_menuCheck, wxT("Check system"));
@@ -83,31 +82,25 @@ MainFrame::MainFrame(wxWindow *parent, wxWindowID id, const wxString &title, con
   m_MainStatusBar->SetStatusText("No project open.");
 
   // Connect Events
-  m_menuFile->Bind(wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(MainFrame::OnClose), this,
-                   m_menuFileClose->GetId());
+  m_menuFile->Bind(
+    wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(MainFrame::OnClose), this, m_menuFileClose->GetId());
 
-  m_menuFile->Bind(wxEVT_COMMAND_MENU_SELECTED,
-                   wxCommandEventHandler(MainFrame::startNewProject), this,
-                   m_menuProjectNew->GetId());
+  m_menuFile->Bind(
+    wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(MainFrame::startNewProject), this, m_menuProjectNew->GetId());
 }
 
 void MainFrame::OnClose(wxCommandEvent &event) { Close(true); }
 
 void MainFrame::startNewProject(wxCommandEvent &event)
 {
-  wxString pythonString{"Python version: "};
+  wxString pythonString{ "Python version: " };
   pythonString.append(m_pythonVersion);
-  if (GetPythonVersion() < 3)
-  {
-    pythonString.append(" (version required 3+ - document generation inhibited.");
-  }
+  if (GetPythonVersion() < 3) { pythonString.append(" (version 3+ required - document generation inhibited."); }
   SetupDialog setupDialog(this, pythonString);
-  if (setupDialog.ShowModal() == wxID_OK)
-  {
+  
+  if (setupDialog.ShowModal() == wxID_OK) {
     wxLogMessage("Setup dialog returned OK");
-  }
-  else
-  {
+  } else {
     wxLogMessage("Setup dialog box was cancelled");
   }
 }
@@ -118,21 +111,16 @@ void MainFrame::OnCheckPython()
   std::string version = "";
 
   FILE *pipe = popen(command.c_str(), "r");
-  if (!pipe)
-  {
+  if (!pipe) {
     wxMessageBox("Failed to get Python information from the system");
     return;
   }
 
   char buffer[128];
-  while (fgets(buffer, sizeof(buffer), pipe) != nullptr)
-  {
-    version += buffer;
-  }
+  while (fgets(buffer, sizeof(buffer), pipe) != nullptr) { version += buffer; }
 
   int status = pclose(pipe);
-  if (status == -1)
-  {
+  if (status == -1) {
     std::cerr << "Failed to close pipe!" << std::endl;
     return;
   }
@@ -143,13 +131,11 @@ void MainFrame::OnCheckPython()
 
   m_pythonVersion = match.str();
 
-  if (version.empty() | std::stoi(m_pythonVersion) < 3)
-  {
+  if (version.empty() | std::stoi(m_pythonVersion) < 3) {
     m_pythonInstalled = false;
-    wxMessageBox("Python version 3 or greater is required for this program.  Document generation step will be disabled.");
-  }
-  else
-  {
+    wxMessageBox(
+      "Python version 3 or greater is required for this program.  Document generation step will be disabled.");
+  } else {
     m_pythonInstalled = true;
     wxString message = "Python version " + m_pythonVersion + " found.";
   }
@@ -157,11 +143,7 @@ void MainFrame::OnCheckPython()
 
 int MainFrame::GetPythonVersion()
 {
-  if (m_pythonVersion.empty())
-  {
-    return 0;
-  }
+  if (m_pythonVersion.empty()) { return 0; }
 
   return std::stoi(m_pythonVersion);
-
 }
--- apps/app.h
+++ apps/app.h
@@ -16,15 +16,16 @@
 #include <wx/wx.h>
 #endif
 
-#include <wx/log.h>
 #include <wx/dirdlg.h>
+#include <wx/log.h>
 
 #include "setupdialog.h"
 
 /**
  * @brief Generate the wxWidgets program.
  */
-class MyApp : public wxApp {
+class MyApp : public wxApp
+{
 public:
   virtual bool OnInit() override;
 };
@@ -34,7 +35,8 @@ public:
  *
  * The window consists of a menu, status bar and blank are for use by the program subsystem.
  */
-class MainFrame : public wxFrame {
+class MainFrame : public wxFrame
+{
 public:
   /**
    * @brief Single constructor for the main frame
@@ -49,35 +51,37 @@ public:
    * The frame is created with wxDEFAULT_FRAME_STYLE | wxMAXIMIZE_BOX | wxMINIMIZE_BOX |
    * wxTAB_TRAVERSAL which allows for resizing the window and for tab cycling.
    */
-  MainFrame( wxWindow *parent = nullptr, wxWindowID id = wxID_ANY,
-             const wxString &title = wxEmptyString, const wxPoint &pos = wxDefaultPosition,
-             const wxSize &size = wxDefaultSize,
-             long style = wxDEFAULT_FRAME_STYLE | wxMAXIMIZE_BOX | wxMINIMIZE_BOX |
-                          wxTAB_TRAVERSAL );
+  MainFrame(wxWindow *parent = nullptr,
+    wxWindowID id = wxID_ANY,
+    const wxString &title = wxEmptyString,
+    const wxPoint &pos = wxDefaultPosition,
+    const wxSize &size = wxDefaultSize,
+    long style = wxDEFAULT_FRAME_STYLE | wxMAXIMIZE_BOX | wxMINIMIZE_BOX | wxTAB_TRAVERSAL);
 
 
   /**
    * @brief destroy any frame based objects not taken care of by the wxWidget class.
    */
-  ~MainFrame() {
-      wxLog::SetActiveTarget(nullptr);
-      delete logger;
+  ~MainFrame()
+  {
+    wxLog::SetActiveTarget(nullptr);
+    delete logger;
   }
 
   /**
    * @brief Close the project down gracefully;
    * @param event The object requesting close
    */
-  virtual void OnClose( wxCommandEvent &event );
+  virtual void OnClose(wxCommandEvent &event);
 
 
   /**
    * @brief Starts a new project
    * @param event The object requesting a new project
-   * 
+   *
    * Creates a setup dialog and hands over control to set up a new project.
    */
-  virtual void startNewProject( wxCommandEvent &event );
+  virtual void startNewProject(wxCommandEvent &event);
 
   /**
    * @brief query the system to determine the Python version in use.
@@ -90,9 +94,9 @@ public:
    * @return major version or zero if python was not discovered.
    */
   int GetPythonVersion();
-  
+
 private:
-  wxLog * logger;
+  wxLog *logger;
   wxMenuBar *m_MainFrameMenu;
   wxMenu *m_menuFile;
   wxMenu *m_menuCheck;
@@ -101,5 +105,4 @@ private:
 
   bool m_pythonInstalled{ false };
   std::string m_pythonVersion{};
-
 };
